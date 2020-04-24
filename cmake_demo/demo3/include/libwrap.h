6db9c93b67069d0e71f318630023f227d772bf9d
happyAnger6@163.com <happyAnger6@163.com>
2020-04-24 21:24:36 +0800

    init

---
+++ cmake_demo/demo3/include/libwrap.h
@@ -0,0 +1,6 @@
+#ifndef _LIB_WRAP_H
+#define _LIB_WRAP_H
+
+extern void prt_num(int);
+
+#endif
---
+++ cmake_demo/demo3/lib/libwrap.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void prt_num(int num)
+{
+	int i = 0;
+	for(i = 0; i < num; i++)
+	{
+		printf("%d\r\n", i);
+	}
+}
